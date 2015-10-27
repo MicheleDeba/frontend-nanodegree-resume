5e8247e3a641fd040061dbde5671d256f7a9f58e
Michele Deba <micheledebattista@digimade.it>
2015-10-28 00:29:33 +0100

    Course edits

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+var formattedName = HTMLheaderName.replace("%data%", "Michele De Battista");
+
+var role = "Web Designer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
